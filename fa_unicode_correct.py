1f12ee4807a3548f94c4444e2a4739441dcd24f2
2i3r <violiance@gmail.com>
2017-07-12 14:14:41 +0000

    indent and spell corrections

--- fa_unicode_correct.py
+++ fa_unicode_correct.py
@@ -1,11 +1,11 @@
 
 trans_table = {
-	u'\u0643': u'\u6a9',
+	u'\u0643': u'\u06a9',
 	u'\u0649': u'\u06cc',
 	u'\u064a': u'\u06cc',
 	u'\u0626': u'\u06cc',
 }
 
 def fa_unicode_correct(s):
-	for k in trans_table.keys();
-		s = s.replace(k, trans_table[k]);
+    for k in trans_table.keys():
+        s = s.replace(k, trans_table[k]);
